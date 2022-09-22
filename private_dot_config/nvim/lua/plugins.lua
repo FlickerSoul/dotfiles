64c953f36d2599ef4291e05791d542c253953a18
FlickerSoul <13360148+FlickerSoul@users.noreply.github.com>
2022-09-21 19:48:34 -0700

    Init

---
+++ private_dot_config/nvim/lua/plugins.lua
@@ -0,0 +1,34 @@
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+
+return require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim' -- Package manager
+  use 'neovim/nvim-lspconfig' -- Collection of configurations for built-in LSP client
+  use "williamboman/nvim-lsp-installer"  -- the lsp installer 
+  use 'hrsh7th/nvim-cmp' -- Autocompletion plugin
+  use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
+  use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
+  use 'L3MON4D3/LuaSnip' -- Snippets plugin
+  use 'numToStr/Comment.nvim'
+  use "lukas-reineke/indent-blankline.nvim"
+  use 'nvim-treesitter/nvim-treesitter'
+  use 'nvim-lualine/lualine.nvim'
+  -- use 'mhartington/formatter.nvim'
+  
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)
