ddc732881ce3824d8ebbe47442d586dfd9976743
Heyuan Zeng <hi@universe.observer>
2023-09-29 00:28:36 +0100

    fix fold with plugins

--- private_dot_config/lvim/config.lua
+++ private_dot_config/lvim/config.lua
@@ -349,6 +349,14 @@ lvim.plugins = {
     end
   },
   { 'keith/swift.vim' },
+  {
+    'kevinhwang91/nvim-ufo',
+    dependencies = { "kevinhwang91/promise-async" }
+  },
+  {
+    "anuvyklack/fold-preview.nvim",
+    dependencies = { "anuvyklack/keymap-amend.nvim" }
+  }
 }
 
 -- rainbow parentheses
@@ -442,6 +450,46 @@ lsp.lua_ls.setup {}
 -- setup telescope
 lvim.builtin.telescope.defaults.file_ignore_patterns = { "node_modules", ".git" }
 
+-- fold with ufo
+vim.keymap.set('n', 'zO', require('ufo').openAllFolds)
+vim.keymap.set('n', 'zC', require('ufo').closeAllFolds)
+
+vim.keymap.set('n', 'zo', "<cmd>foldopen<CR>")
+vim.keymap.set('n', 'zc', "<cmd>foldclose<CR>")
+
+
+require('ufo').setup({
+  provider_selector = function(bufnr, filetype, buftype)
+    return ""
+  end
+})
+
 -- fold with treesitter
 vim.opt.foldmethod = "expr"
-vim.opt.foldexpr = "v:lua.vim.treeesitter.foldexpr()"
+vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+
+-- local vim = vim
+-- local api = vim.api
+-- local M = {}
+-- -- function to create a list of commands and convert them to autocommands
+-- -------- This function is taken from https://github.com/norcalli/nvim_utils
+-- function M.nvim_create_augroups(definitions)
+--   for group_name, definition in pairs(definitions) do
+--     api.nvim_command('augroup ' .. group_name)
+--     api.nvim_command('autocmd!')
+--     for _, def in ipairs(definition) do
+--       local command = table.concat(vim.tbl_flatten { 'autocmd', def }, ' ')
+--       api.nvim_command(command)
+--     end
+--     api.nvim_command('augroup END')
+--   end
+-- end
+
+-- local autoCommands = {
+--   -- other autocommands
+--   open_folds = {
+--     { "BufReadPost,FileReadPost", "*", "normal zR" }
+--   }
+-- }
+
+-- M.nvim_create_augroups(autoCommands)
